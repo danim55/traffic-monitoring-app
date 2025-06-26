02a107b87e94afc55c40376fda3edf525590859e
dani <daniel.merino@thalesaleniaspace.com>
2025-06-27 00:10:33 +0200

    Add environment variable load and check method, add entrypoint method, initialize variables with environment variables values

--- state-init/state_init/main.py
+++ state-init/state_init/main.py
@@ -1,6 +1,28 @@
-def main():
-    print("Hello, World!")
+import os
+import sys
+
+
+def _get_required_env_var(var_name: str) -> str:
+    value = os.getenv(var_name)
+    if not value:
+        sys.exit(f"Missing required environment variable {var_name}")
+    return value
+
+
+def entry_point() -> None:
+    # Opensearch variable envs
+    opensearch_host = _get_required_env_var("STATEINIT_OPENSEARCH_HOST")
+    opensearch_port = _get_required_env_var("STATEINIT_OPENSEARCH_PORT")
+    opensearch_dashboard_host = _get_required_env_var(
+        "STATEINIT_OPENSEARCH_DASHBOARDS_HOST"
+    )
+    opensearch_dashboards_port = _get_required_env_var(
+        "STATEINIT_OPENSEARCH_DASHBOARDS_PORT"
+    )
+    opensearch_dashboard_conf_file = _get_required_env_var(
+        "STATEINIT_OPENSEARCH_DASHBOARDS_RESOURCES_PATH"
+    )
 
 
 if __name__ == "__main__":
-    main()
+    entry_point()
