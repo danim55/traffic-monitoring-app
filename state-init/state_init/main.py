8972601e6979a339c82906dd1f6100c0115f8aa0
dani <daniel.merino@thalesaleniaspace.com>
2025-06-28 21:26:25 +0200

    Add example 2 to index-mappings.json, create all indices from example

--- state-init/state_init/main.py
+++ state-init/state_init/main.py
@@ -1,5 +1,10 @@
+import json
 import os
 import sys
+
+from opensearch.opensearch_service import create_index
+
+INDEX_MAPPINGS_FILE_NAME = "index-mappings.json"
 
 
 def _get_required_env_var(var_name: str) -> str:
@@ -21,6 +26,16 @@ def entry_point() -> None:
         "STATEINIT_OPENSEARCH_DASHBOARDS_RESOURCES_PATH"
     )
 
+    load_opensearch_idices(opensearch_dashboard_conf_file)
+
+
+def load_opensearch_idices(dashboards_conf_file: str) -> None:
+    # Load indices from json file
+    with open(f"{dashboards_conf_file}/{INDEX_MAPPINGS_FILE_NAME}") as json_file:
+        index_mappings = json.load(json_file)
+
+    for index_name, mappings in index_mappings.items():
+        create_index(index_name, mappings)
 
 if __name__ == "__main__":
     entry_point()
