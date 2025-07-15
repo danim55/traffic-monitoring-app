0250d069e8a3044fa71be34c7e8c5f3f579977ad
dani <daniel.merino@thalesaleniaspace.com>
2025-07-15 17:29:11 +0200

    Sniff packets from eth0 continuously and print packet

--- detector/detector/main.py
+++ detector/detector/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello, World!")
+import pyshark
+
+
+def capture_traffic() -> None:
+    capture = pyshark.LiveCapture(interface="eth0")
+    capture.sniff_continuously()
+
+    for pkt in capture:
+        print(pkt)
 
 
 if __name__ == "__main__":
-    main()
+    capture_traffic()
