4be8806b8bdb6d42d9a2cef343cf75d7be106108
dani <daniel.merino@thalesaleniaspace.com>
2025-07-17 23:02:23 +0200

    Capture packages

--- neural-network/neural_network/main.py
+++ neural-network/neural_network/main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello")
+from datetime import datetime
+
+import pyshark
+
+
+def test_pyshark() -> None:
+    capture = pyshark.LiveCapture(interface="eth0")
+
+    for pkt in capture.sniff_continuously():
+        ts = datetime.fromtimestamp(float(pkt.sniff_timestamp))
+        print(pkt)
+        print(ts)
 
 
 if __name__ == "__main__":
-    main
+    test_pyshark()
