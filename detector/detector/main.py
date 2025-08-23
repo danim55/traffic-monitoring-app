2bfd8756cd1400ae07b9617cba2af659788bf1cf
dani <daniel.merino@thalesaleniaspace.com>
2025-08-23 13:26:55 +0200

    Remove pyshark dependency

--- detector/detector/main.py
+++ detector/detector/main.py
@@ -1,17 +1,6 @@
-import pyshark
-
-
 def dummy_test_aux(value: int) -> int:
     return value
 
 
-def capture_traffic() -> None:
-    capture = pyshark.LiveCapture(interface="eth0")
-    capture.sniff_continuously()
-
-    for pkt in capture:
-        print(len(pkt))
-
-
 if __name__ == "__main__":
-    capture_traffic()
+    print("Hello world")
